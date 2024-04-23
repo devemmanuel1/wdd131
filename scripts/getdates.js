cd4b3be3d5fb23419218ecb0f6e0364e2e473639
devemmanuel1 <eze23010@byui.edu>
2024-04-23 19:50:15 +0100

    week 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,12 +1,23 @@
-document.addEventListener("DOMContentLoaded", function() {
+// document.addEventListener("DOMContentLoaded", function() {
 
-    // const currentYear = new Date().getFullYear();
-    let oLastModif = new Date(document.lastModified);
-    document.getElementById("currentYear").textContent = currentYear;
+//     // const currentYear = new Date().getFullYear();
+//     let oLastModif = new Date(document.lastModified);
+//     document.getElementById("currentYear").textContent = currentYear;
 
 
-    const lastModified = document.lastModified;
-    document.getElementById("lastModified").textContent = lastModified;
-});
-const currentYear = new Date().getFullYear(); 
-const yearElement = document.querySelector('#year');
+//     const lastModified = document.lastModified;
+//     document.getElementById("lastModified").textContent = lastModified;
+// });
+// const currentYear = new Date().getFullYear(); 
+// const yearElement = document.querySelector('#year');
+
+
+const currentYear = new Date().getFullYear();
+
+const copyrightParagraph = document.querySelector("currentyear");
+copyrightParagraph.textContent = `© ${currentYear} Your Company Name`;
+
+const lastModifiedDate = document.lastModified;
+
+const lastModifiedParagraph = document.querySelector("currentyear(2)");
+lastModifiedParagraph.textContent = `Last modified: ${lastModifiedDate}`;

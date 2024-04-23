47dd079fb1d05e0ed5462476aed7c5a33ad50ba6
devemmanuel1 <eze23010@byui.edu>
2024-04-23 20:24:34 +0100

    week 1

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,23 +1,5 @@
-// document.addEventListener("DOMContentLoaded", function() {
-
-//     // const currentYear = new Date().getFullYear();
-//     let oLastModif = new Date(document.lastModified);
-//     document.getElementById("currentYear").textContent = currentYear;
-
-
-//     const lastModified = document.lastModified;
-//     document.getElementById("lastModified").textContent = lastModified;
-// });
-// const currentYear = new Date().getFullYear(); 
-// const yearElement = document.querySelector('#year');
-
-
 const currentYear = new Date().getFullYear();
+const lastModifiedDate = new Date(document.lastModified);
 
-const copyrightParagraph = document.querySelector("currentyear");
-copyrightParagraph.textContent = `© ${currentYear} Your Company Name`;
-
-const lastModifiedDate = document.lastModified;
-
-const lastModifiedParagraph = document.querySelector("currentyear(2)");
-lastModifiedParagraph.textContent = `Last modified: ${lastModifiedDate}`;
+document.getElementById('currentyear').innerText = currentYear;
+document.getElementById('lastModified').innerText = "Last Modified: " + lastModifiedDate;
